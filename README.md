275a56514aeed76079b6e1d38e598fe5222e6c36
Muhammad Rizwan <m.rizwanmeo7862@gmail.com>
2020-12-05 14:50:43 +0500

    something

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# flutter_app
+# dice_app
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,99 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:bmi_calculator/screens/input_page.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(BMICalculator());
+void main() {
+  return runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashWidget(),
+  ));
+}
 
-class BMICalculator extends StatelessWidget {
+class SplashWidget extends StatefulWidget {
+  SplashWidget({Key key}) : super(key: key);
+
+  @override
+  _SplashWidgetState createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new DicePage(),
+      title: new Text(
+        'FA17-BSE-008 (Farman Ali)',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
       ),
-      home: InputPage(),
+      image: Image.asset('assets/images/farman.jpg'),
+      photoSize: 100,
+      backgroundColor: Colors.white,
+      loaderColor: Colors.red,
+    );
+  }
+}
+
+class AppLoader extends StatelessWidget {
+  // const AppLoader({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Dice Game'),
+        backgroundColor: Colors.blue,
+      ),
+      body: DicePage(),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'assets/images/dice$leftDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'assets/images/dice$rightDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
